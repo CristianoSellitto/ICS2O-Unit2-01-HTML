5c77ded055a2ff18bf5ce0c0eb6b4fa04c53ebde
CristianoSellit <99354780+CristianoSellitto@users.noreply.github.com>
2022-03-08 18:13:42 +0000

    Added a button that creates text.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Cristiano | ICS2O-Unit2-01-HTML | March 8 2022
+
 function onButtonClick() {
-  alert("Hello, World!")
+  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
 }
